88a733f5d8e62a8a1ea24f5be454009d6b8ddc47
ThomasTadesse <328704@student.mboutrecht.nl>
2025-03-05 17:37:44 +0100

    sp improvements

--- Magazijn_Jamin/database/migrations/2025_02_17_225535_create_stored_procedure_producten.php
+++ Magazijn_Jamin/database/migrations/2025_02_17_225535_create_stored_procedure_producten.php
@@ -12,11 +12,59 @@ return new class extends Migration
     public function up(): void
     {
         DB::unprepared('
-        
+        DROP PROCEDURE IF EXISTS spGetProducts;
+        CREATE PROCEDURE spGetProducts()
+        BEGIN
+            SELECT 
+                ALRG.id               AS AllergeenId,
+                PRCT.Naam            AS ProductNaam,
+                LVR.Naam             AS LeverancierNaam,
+                LVR.Contactpersoon   AS Contactpersoon,
+                MGZN.AantalAanwezig  AS AantalAanwezig
+            FROM 
+                Allergeen ALRG
+            JOIN 
+                ProductPerAllergeen PRAL ON ALRG.Id = PRAL.AllergeenId
+            JOIN 
+                Product PRCT ON PRAL.ProductId = PRCT.Id
+            LEFT JOIN 
+                Magazijn MGZN ON PRCT.Id = MGZN.ProductId
+            LEFT JOIN
+                ProductPerLeverancier PPL ON PRCT.Id = PPL.ProductId
+            LEFT JOIN
+                Leverancier LVR ON PPL.LeverancierId = LVR.Id
+            WHERE 
+                ALRG.IsActief = 1 
+                AND PRCT.IsActief = 1
+            ORDER BY 
+                LeverancierNaam ASC;
+        END;
         ');
 
         DB::unprepared('
-        
+            DROP PROCEDURE IF EXISTS spGetProductDetails;
+            CREATE PROCEDURE spGetProductDetails()
+            BEGIN
+                SELECT 
+                    PRCT.Naam            AS ProductNaam,
+                    ALRG.Naam            AS AllergeenNaam,
+                    PPL.DatumLevering    AS DatumLevering,
+                    PPL.Aantal           AS Aantal
+                FROM 
+                    Product PRCT
+                JOIN 
+                    ProductPerAllergeen PPA ON PRCT.Id = PPA.ProductId
+                JOIN 
+                    Allergeen ALRG ON PPA.AllergeenId = ALRG.Id
+                LEFT JOIN 
+                    ProductPerLeverancier PPL ON PRCT.Id = PPL.ProductId
+                WHERE 
+                    PRCT.IsActief = 1
+                    AND ALRG.IsActief = 1
+                ORDER BY 
+                    PRCT.Naam ASC,
+                    ALRG.Naam ASC;
+            END;
         ');
     }
 
@@ -25,7 +73,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        DB::unprepared('DROP PROCEDURE IF EXISTS ');
-        DB::unprepared('DROP PROCEDURE IF EXISTS ');
+        DB::unprepared('DROP PROCEDURE IF EXISTS spGetProducts;');
+        DB::unprepared('DROP PROCEDURE IF EXISTS spGetProductDetails;');
     }
 };
